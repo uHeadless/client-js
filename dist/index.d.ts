d11692355b80a784d5ab007d22b37cde268204b6
Thomas Kjærgaard <tkj@tkjaergaard.dk>
2021-03-16 15:15:00 +0100

    ignore dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export default class UHeadlessClient {
-    token: string;
-    cdnUrl: string;
-    constructor(token: string, cdnUrl: string);
-    private getUrl;
-    fetch(options?: any): Promise<any>;
-}
